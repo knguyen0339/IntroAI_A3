07943c7ca65ab7eae279ea474f3384c0f95dcf8a
knguyen0339 <91980470+knguyen0339@users.noreply.github.com>
2022-10-04 16:03:57 -0700

    Update backtrack.py

--- sudoku-for-students/backtrack.py
+++ sudoku-for-students/backtrack.py
@@ -1,3 +1,12 @@
+We the undersigned promise that we have in good faith attempted to follow the 
+principles of pair programming. Although we were free to discuss ideas with 
+others, the implementation is our own. We have shared a common workspace 
+(possibly virtually) and taken turns at the keyboard for the majority of the 
+work that we are submitting. Furthermore, any non programming portions of the 
+assignment were done independently. We recognize that should this not be the 
+case, we will be subject to penalties as outlined in the course syllabus. 
+[Phillip Cuesta and Kaitlyn Nguyen]
+
 
 
 from csp_lib.backtrack_util import (first_unassigned_variable, 
